8849d408e22ed27040d6cb2b02e455fdad42ff92
luzhangting <luzhangting@p1.com>
2023-09-25 22:08:50 +0800

    初始化router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <h1>Vue</h1>
-  <h1 class="text-3xl font-bold underline">Hello World!</h1>
+  <RouterView />
 </template>
 <script setup></script>
 <style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1 class="text-3xl font-bold bg-slate-300">Hello World!</h1>
+</template>
+<script setup></script>
